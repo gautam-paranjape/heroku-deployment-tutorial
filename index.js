2c756212ddbee890fc4fd54169f89539d5504648
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2020-11-16 19:41:45 -0800

    push

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { token } = require("./token.json");
 
 client.on("ready", () => {
   console.log(`Bot ready`);
@@ -12,4 +11,4 @@ client.on("message", (message) => {
   }
 });
 
-client.login(token);
+client.login(process.env.token);
